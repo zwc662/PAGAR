ee2f13f25df038d30e5a559cc743dccc4de39e81
zwc662 <zwc662@gmail.com>
2023-05-23 11:21:25 -0400

    upda

--- minigrid/rl-starter-files/utils/env.py
+++
@@ -1,9 +0,0 @@
-import gym, gym_minigrid
-
-
-def make_env(env_key, seed=None, render_mode=None):
-    env = gym.make(env_key)#, render_mode=render_mode)
-    env.seed(seed)
-    env.reset()
-    return env
-
