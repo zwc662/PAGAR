b455e31e88c431173e9ffc2d9d1c248e118e9baa
zwc662 <zwc662@gmail.com>
2023-09-28 19:07:42 -0400

    update

--- run.sh
+++ run.sh
@@ -11,12 +11,12 @@ cd $alg
 if [ $env == 'minigrid' ];
 then 
     tensorboard --logdir scripts/logs &
-    if [ $task != 'MiniGrid-DoorKey-6x6-v0' ];
-    then
-        python -m scripts.train --env ${task} --no-cuda --entropy --demonstration ../expert_demo/exp_demo_${task}_${demos}ep.p
-    elif [ $task != 'MiniGrid-FourRooms-v0' ];
+    if [ $task = 'MiniGrid-FourRooms-v0' ];
     then
         python -m scripts.train --env ${task} --no-cuda --entropy --demonstration ../expert_demo/exp_demo_SimpleCrossingS9N1-v0.p
+    elif [ $task != 'MiniGrid-DoorKey-6x6-v0' ];
+    then
+        python -m scripts.train --env ${task} --no-cuda --entropy --demonstration ../expert_demo/exp_demo_${task}_${demos}ep.p
     else
         python -m scripts.train --env ${task} --no-cuda --demonstration ../expert_demo/exp_demo_${task}_${demos}ep.p
     fi
