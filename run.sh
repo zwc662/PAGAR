eae270c4000e597d82c9464598c07b8490184f03
zwc662 <zwc662@gmail.com>
2023-09-28 18:51:36 -0400

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 env=$1
 task=$2
 alg=$3
+demos=$4
 echo Your container args are: "$@"
 
 cd $env
@@ -12,9 +13,9 @@ then
     tensorboard --logdir scripts/logs &
     if [ $task != 'MiniGrid-DoorKey-6x6-v0' ];
     then
-        python -m scripts.train --env $task --no-cuda --entropy
+        python -m scripts.train --env $task --no-cuda --entropy --demonstration ../expert_demo/expert_demo_$task-v0_($demos)ep.p
     else
-        python -m scripts.train --env $task --no-cuda
+        python -m scripts.train --env $task --no-cuda --demonstration ../expert_demo/expert_demo_$task-v0_($demos)ep.p
     fi
     #tensorboard --logdir ./logs 
 else
